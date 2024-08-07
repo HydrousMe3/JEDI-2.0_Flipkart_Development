87aea4a83b3cbcf984ef1f3cef0361a900e6a819
HydrousMe3 <shubham.abhaylulu@flipkart.com>
2024-08-07 22:33:56 +0530

    gradecard

--- CRS-FLIPKART-GROUP-F-JAVA-POS/src/com/flipkart/bean/GradeCard.java
+++ CRS-FLIPKART-GROUP-F-JAVA-POS/src/com/flipkart/bean/GradeCard.java
@@ -1,8 +1,14 @@
 package com.flipkart.bean;
 
-public class Course {
-    private String courseCode;
-    private String name;
-    private String instructor;
-    private Boolean isOffered;
+import java.util.List;
+
+public class GradeCard{
+    private int studentId;
+    private int semester;
+    private float cpi;
+    private List<RegisteredCourses> registeredCourses;
+
+    public float calculateCpi(){
+        return 1.0f;
+    }
 }
