7172221397a80a99a248c80591a8444137f5a210
HydrousMe3 <shubham.abhaylulu@flipkart.com>
2024-08-07 22:18:46 +0530

    bean

--- CRS-FLIPKART-GROUP-F-JAVA-POS/src/com/flipkart/business/CustomerBusiness.java
+++ CRS-FLIPKART-GROUP-F-JAVA-POS/src/com/flipkart/business/CustomerBusiness.java
@@ -1,30 +1,18 @@
 package com.flipkart.business;
 
 public class CustomerBusiness {
-
-	// define the business Methods
-
-	// create Customer
-	public void createCustomer() {
-
-		System.out.println("Customer is created here--->");
-	}
-
-	public boolean deleteCustomer(int customerId) {
-
-		System.out.println("Customer is deleted with Id-->" + customerId);
-		return true;
-	}
-
-	public boolean updateCustomer(int customerId) {
-
-		System.out.println("Customer is updated with id-->" + customerId);
-		return true;
-	}
-	
-	public void listCustomer() {
-
-		System.out.println("Details of customer -->");
-	}
-
+    public void createCustomer(){
+        System.out.println("customer is created  here--->");
+    }
+    public boolean deleteCustomer(int customerId){
+        System.out.println("customer is deleted with iD-->"+customerId);
+        return true;
+    }
+    public boolean updateCustomer(int customerId){
+        System.out.println("Customer is updated with iD-->"+customerId);
+        return true;
+    }
+    public void listCustomer(){
+        System.out.println("details of customer listed here");
+    }
 }
--- CRS-FLIPKART-GROUP-F-JAVA-POS/src/com/flipkart/client/CustomerClient.java
+++ CRS-FLIPKART-GROUP-F-JAVA-POS/src/com/flipkart/client/CustomerClient.java
@@ -1,22 +1,13 @@
 package com.flipkart.client;
 
 import com.flipkart.business.CustomerBusiness;
-
+//client layer which call all business methods in customer application
 public class CustomerClient {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		CustomerBusiness business=new CustomerBusiness();
-	    business.createCustomer();
-		System.out.println("delete customer by Id--." +business.deleteCustomer(101));
-		System.out.println("Update Customer by id-->" +business.updateCustomer(101));
-		business.listCustomer();
-		
-
-	}
-
+    public static void main(String[] args) {
+        CustomerBusiness business=new CustomerBusiness();
+        business.createCustomer();
+        System.out.println("delete customer by id--"+business.deleteCustomer(101));
+        System.out.println("update customer by id--"+business.updateCustomer(101));
+        business.listCustomer();
+    }
 }
